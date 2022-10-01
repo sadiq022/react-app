2791890ebded907ceaa322164c214f5c3340ab10
“sadiq022” <sadiqali8791@gmail.com>
2022-10-02 00:25:44 +0530

    Removed routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 
 import React, { useState } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 
 function App() {
@@ -52,16 +52,17 @@ function App() {
 
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
-      <div className="container my-3">
+      {/* <div className="container my-3">
         <Routes>
               <Route exact path="/about" element={<About/>} />
               <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze!" mode={mode}/>} />
         </Routes>
-      </div>
-    </Router>
+      </div> */}
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze!" mode={mode}/>
+    {/* </Router> */}
     </>
   );
 }
